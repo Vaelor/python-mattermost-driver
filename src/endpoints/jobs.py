7a852d9e9216e1f68f1e16fc417388c086d08b17
Christian Plümer <christian.pluemer@kuuku.net>
2017-06-23 16:17:28 +0200

    Implement jobs endpoints

---
+++ src/endpoints/jobs.py
@@ -0,0 +1,16 @@
+from src.endpoints.base import Base
+
+
+class Jobs(Base):
+	endpoint = '/jobs'
+
+	def get_status_of_job(self, job_id):
+		return self.client.get(
+			self.endpoint + '/' + job_id + '/status'
+		)
+
+	def get_status_of_jobs(self, type, params=None):
+		return self.client.get(
+			self.endpoint + '/type/' + type + '/statuses',
+			params=params
+		)
