ee854a91268b0ee6360cbb267c3a25f08bc66e6a
Christian Plümer <christian.pluemer@kuuku.net>
2017-06-23 16:22:51 +0200

    Implement brand endpoint
    fix #11

---
+++ src/endpoints/brand.py
@@ -0,0 +1,16 @@
+from src.endpoints.base import Base
+
+
+class Brand(Base):
+	endpoint = '/brand'
+
+	def get_brand_image(self):
+		return self.client.get(
+			self.endpoint + '/image'
+		)
+
+	def upload_brand_image(self, data=None):
+		return self.client.post(
+			self.endpoint + '/image',
+			data=data
+		)
