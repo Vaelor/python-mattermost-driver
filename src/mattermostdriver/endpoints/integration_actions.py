5cfc535cf5c6c2c1665186f7e31c0ef917c36be7
rittelle <33598633+rittelle@users.noreply.github.com>
2020-07-29 22:09:00 +0200

    added integration_actions endpoint
    https://api.mattermost.com/#tag/integration_actions
    
    submit_dialog() is untested.

---
+++ src/mattermostdriver/endpoints/integration_actions.py
@@ -0,0 +1,16 @@
+from .base import Base
+
+class IntegrationActions(Base):
+	endpoint = '/actions'
+
+	def open_dialog(self, options):
+		return self.client.post(
+			self.endpoint + '/dialogs/open',
+			options=options
+		)
+
+	def submit_dialog(self, options):
+		return self.client.post(
+			self.endpoint + '/dialogs/open',
+			options=options
+		)
